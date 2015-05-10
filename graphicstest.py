3a248cb3d0a675f01b3c9eb2e13f5a031bd29586
Eric Dennison <ericd@netdenizen.com>
2015-05-10 14:41:35 -0400

    Updated from Brython Server: 5/10/2015 2:41:35 PM

--- graphicstest.py
+++ graphicstest.py
@@ -1,1 +1,16 @@
+from browser import window, document
+
+def onload(window):
+    canvas = window.document.createElement('canvas')
+    canvas.setAttribute('width','800')
+    canvas.setAttribute('height','500')
+    canvas.setAttribute('style','border:1px solid #000000;')
+    window.document.body.appendChild(canvas)
+    
+
+w = window.open("", "")
+w.onload = onload(w)
+
+
 print("Testing Graphics")
+
