c30f94918636c4942581dcf44f33ae54b6f6c810
Eric Dennison <ericd@netdenizen.com>
2015-07-30 14:01:36 -0400

    Updated from Brython Server: 7/30/2015 2:01:35 PM

--- s.py
+++ s.py
@@ -10,7 +10,8 @@ class Bunny(Sprite):
         self.app.listenMouseEvent(MouseEvent.mousedown, self.mousedown)
         self.app.listenMouseEvent(MouseEvent.mouseup, self.mouseup)
         self.app.listenMouseEvent(MouseEvent.mousemove, self.mousemove)
-        
+        self.dragging = False
+
     
     def step(self):
         """
@@ -21,13 +22,34 @@ class Bunny(Sprite):
             self.y += randint(-50,50)
         
     def mousedown(self, event):
-        
-        
+        # capture any mouse down within 50 pixels
+        self.deltax = self.x - event.x 
+        self.deltay = self.y - event.y
+        if abs(self.deltax) < 50 and abs(self.deltay) < 50:
+            self.dragging = True
+            event.consumed = True
+            
+    def mousemove(self, event):
+        if self.dragging:
+            self.x = event.x + self.deltax
+            self.y = event.y + self.deltay
+            event.consumed = True
+            
+    def mouseup(self, event):
+        if self.dragging:
+            self.dragging = False
+            event.consumed = True
+            
         
 class DemoApp(App):
     
     def __init__(self):
         super().__init__(self, 500, 500)
+        # Create an image asset for the app
+        bunny = ImageAsset(self, "ggame/bunny.png")
+        # Create several bunnies
+        for i in range(10):
+            Bunny(bunny, (randint(50,450),randint(50,450)))
         
     def step(self):
         """
@@ -39,10 +61,6 @@ class DemoApp(App):
 
 
 # Create the app, with a 500x500 pixel stage
-app = App(500,500)  
-# Create an image asset for the app
-grass = ImageAsset(app, "ggame/bunny.png")
-# Create a displayed object using the asset
-Sprite(grass, (100,100))
+app = DemoApp(500,500)  
 # Run the app
 app.run()
