bbfe2f96813ebf9659c5da4ea9b47c9c17c86a17
Eric Dennison <ericd@netdenizen.com>
2015-08-01 21:20:59 -0400

    Updated from Brython Server: 8/1/2015 9:20:58 PM

--- __main__.py
+++ __main__.py
@@ -1,13 +1,2 @@
 print("Hello from https://github.com/tiggerntatie/brython-server-testing")
 
-class t(object):
-    def __init__(self):
-        self.x = 1
-        
-class u(t):
-    
-    def sety(self, y = self.x):
-        self.y = y
-
-uinst = u()
-print(uinst.y)
