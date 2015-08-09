57524d7fac7a77b802e39eefdf89a21c09d5fcbe
Eric Dennison <ericd@netdenizen.com>
2015-08-09 11:30:42 -0400

    Update classtest.py

--- classtest.py
+++ classtest.py
@@ -1,12 +1,12 @@
 class t0(object):
     def __init__(self):
-        self.zz = top
+        self.zz = 'top'
 
 
 class t1(t0):
     def __init__(self):
         self.x = 5
-        self.z = q
+        self.z = 'q'
         
 class t2(t1):
     def __init__(self):
