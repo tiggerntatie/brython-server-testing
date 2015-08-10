47f3c29f7abfb8a4fd7c2da50707ed3c925e6de6
Eric Dennison <ericd@netdenizen.com>
2015-08-09 22:18:59 -0400

    Update classtest.py

--- classtest.py
+++ classtest.py
@@ -1,17 +1,3 @@
 class t0(object):
     def __init__(self):
         self.zz = top
-
-
-class t1(t0):
-    def __init__(self):
-        super().__init__()
-        self.x = 5
-        self.z = 'q'
-        
-class t2(t1):
-    def __init__(self):
-        super().__init__()
-        self.y = 6
-        
-T = t2()
